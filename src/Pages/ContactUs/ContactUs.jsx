f299cf66a475410bd94d51e22865a0911c09182f
Md Mueid MiaMd-Mueid-Mia <mdmueidmia@gmail.com>
2025-01-14 20:38:16 +0600

    replace images for testimonialSlider

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -33,7 +33,7 @@ const ContactUs = () => {
   };
 
   return (
-    <div className=" py-20 bg-gray-100">
+    <div className=" px-5 py-20 bg-gray-100">
       <h1 className="text-3xl font-bold text-center mb-6">Contact Us</h1>
       <div className="bg-white shadow-lg rounded-lg w-full  p-6">
         <div className="flex flex-col md:flex-row gap-6">
